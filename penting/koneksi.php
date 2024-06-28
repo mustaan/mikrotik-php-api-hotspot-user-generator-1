84d9f7c05032766a848d8c88b28fd2a3d1701453
mustaan <redebrj@gmail.com>
2024-06-29 01:07:41 +0700

    Update koneksi.php

--- penting/koneksi.php
+++ penting/koneksi.php
@@ -2,7 +2,7 @@
 	<?php 
 		 include("routeros_api.php");
 		 $API = new routeros_api();
-		 if (!$API->connect("192.168.10.1","admin","a")){
+		 if (!$API->connect("20.10.10.1","admin","a")){
 			 unset($_SESSION["user"]);
 			 header("location:login.php");
 		 }
